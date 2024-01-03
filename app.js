2a43149dac5d1b5157ca331a14bdce1624258881
kadie972 <codeurfou@outlook.com>
2024-01-03 11:31:49 +0100

    ajout du fichier mock-pokemon.js

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send("hello express 5"))
 
+app.get('/api/pokemons/:id/:name/:color', (req, res) =>{
+const id = req.params.id
+const name = req.params.name
+const color = req.params.color
+res.send(`vous avez demandé le pokemon ${id} dont le nom est ${name} qui est de couleur ${color}`)
+} )
+
 app.listen(port, () => console.log(`Application started on : http://localhost:${port} `))
