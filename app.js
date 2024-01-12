66c196c04e3d6b04bbb89bf009222b0a8577aca8
kadie972 <codeurfou@outlook.com>
2024-01-12 10:36:27 +0100

    ajout des routes operation CRUD

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const bodyParser = require('body-parser');
 const sequelize = require ('./src/db/sequelize');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app
   .use(morgan('dev'))
   .use(bodyParser.json())
 
 sequelize.initDb();
-
-    
+require('./src/routes/findAllPokemons')(app)  
+require('./src/routes/createPokemon')(app)  
+require('./src/routes/findPokemonByPk')(app)  
+require('./src/routes/updatePokemon')(app)  
+require('./src/routes/deletePokemon')(app)  
 
 
 app.listen(port, () => console.log(`Application started on : http://localhost:${port} `))
